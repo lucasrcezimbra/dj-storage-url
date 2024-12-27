c09d60771107a5ec56f5e916140fdd0365a98199
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-12-27 16:38:01 -0300

    0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.0.3 (2024-12-17)
+* fix(FileSystemStorage): relative location
+
 ## 0.0.2 (2024-12-17)
 * First release working minimally.
 
--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ author = "Lucas Rangel Cezimbra"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.0.2"
+version = "0.0.3"
 # The full version, including alpha/beta/rc tags.
 release = version
 
