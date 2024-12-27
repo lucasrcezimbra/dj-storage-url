69dfa5bae0e69514eec5d61af003dc464dd9334c
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-12-27 14:21:09 -0300

    v0

--- dj_storage_url/__init__.py
+++ dj_storage_url/__init__.py
@@ -1,5 +1,59 @@
-"""Top-level package for dj-storage-url."""
+from urllib.parse import parse_qs, urlparse, urlsplit
 
-__author__ = """Lucas Rangel Cezimbra"""
-__email__ = "lucas@cezimbra.tec.br"
-__version__ = "0.0.1"
+
+def strtobool(value):
+    value = value.lower()
+    if value in ("t", "true", "1"):
+        return True
+    elif value in ("f", "false", "0"):
+        return False
+    else:
+        raise ValueError("invalid truth value %r" % (value,))
+
+
+def file_system_storage(url):
+    parsed_url = urlparse(url)
+    return {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "OPTIONS": {
+            "location": parsed_url.path,
+        },
+    }
+
+
+def memory_storage(_):
+    return {"BACKEND": "django.core.files.storage.MemoryStorage", "OPTIONS": {}}
+
+
+def s3_storage(url):
+    parsed_url = urlparse(url)
+    querystring = parse_qs(parsed_url.query)
+    return {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "bucket_name": parsed_url.netloc,
+            **({"location": parsed_url.path} if parsed_url.path else {}),
+            **(
+                {"gzip": strtobool(querystring["gzip"][0])}
+                if "gzip" in querystring
+                else {}
+            ),
+        },
+    }
+
+
+def unknown_storage(url):
+    scheme, _ = url.split("://")
+    return {"BACKEND": scheme, "OPTIONS": {}}
+
+
+SCHEME_TO_CONFIG = {
+    "file": file_system_storage,
+    "memory": memory_storage,
+    "s3": s3_storage,
+}
+
+
+def parse(url):
+    scheme, *_ = urlsplit(url)
+    return SCHEME_TO_CONFIG.get(scheme, unknown_storage)(url)
--- tests/test_dj_storage_url.py
+++ tests/test_dj_storage_url.py
@@ -1,2 +1,81 @@
-def test_dj_storage_url():
-    assert True
+import pytest
+
+from dj_storage_url import parse
+
+
+class TestInMemoryStorage:
+    def test_parse(self):
+        assert parse("memory://") == {
+            "BACKEND": "django.core.files.storage.MemoryStorage",
+            "OPTIONS": {},
+        }
+
+
+class TestFileSystemStorage:
+    def test_parse(self):
+        assert parse("file:///tmp/files") == {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+            "OPTIONS": {
+                "location": "/tmp/files",
+            },
+        }
+
+    @pytest.mark.skip(reason="TODO")
+    def test_with_optional_options(self):
+        url = "file:///tmp/files?base_url=/my/base/url&file_permissions_mode=0o644&directory_permissions_mode=0o644"
+        assert parse(url) == {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+            "OPTIONS": {
+                "location": "/tmp/files",
+                "base_url": "/my/base/url",
+                "file_permissions_mode": 0o644,
+                "directory_permissions_mode": 0o644,
+            },
+        }
+
+
+class TestS3Storage:
+    def test_parse(self):
+        assert parse("s3://my-bucket") == {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "OPTIONS": {
+                "bucket_name": "my-bucket",
+            },
+        }
+
+    def test_with_location(self):
+        assert parse("s3://my-bucket/my-folder") == {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "OPTIONS": {
+                "bucket_name": "my-bucket",
+                "location": "/my-folder",
+            },
+        }
+
+    @pytest.mark.parametrize("gzip", ["True", "true", "1"])
+    def test_gzip_true(self, gzip):
+        assert parse(f"s3://my-bucket?gzip={gzip}") == {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "OPTIONS": {
+                "bucket_name": "my-bucket",
+                "gzip": True,
+            },
+        }
+
+    @pytest.mark.parametrize("gzip", ["False", "false", "0"])
+    def test_gzip_false(self, gzip):
+        assert parse(f"s3://my-bucket?gzip={gzip}") == {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "OPTIONS": {
+                "bucket_name": "my-bucket",
+                "gzip": False,
+            },
+        }
+
+
+class TestCustomStorage:
+    def test_parse(self):
+        assert parse("myproject.myapp.MyCustomStorage://") == {
+            "BACKEND": "myproject.myapp.MyCustomStorage",
+            "OPTIONS": {},
+        }
